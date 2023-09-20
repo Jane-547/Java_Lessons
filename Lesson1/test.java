0cec95863beb67a1ee7be4e502592ddd802f1f90
Jane-547 <Jane-547@bk.com>
2023-09-20 10:27:42 +0300

    Initial commit

---
+++ Lesson1/test.java
@@ -0,0 +1,11 @@
+package Lesson1;
+
+public class test {
+    public static void main(String[] args) {
+        int i = 10;
+        int a = i-- - --i;
+
+        System.out.println(a);
+        System.out.println(i);
+    }
+}
