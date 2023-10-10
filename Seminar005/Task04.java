c2488e15e26bc2eadd099151ae48beb89d705b80
Jane-547 <Jane-547@bk.com>
2023-10-10 06:06:44 +0300

    Seminar 06 added

--- Seminar005/Task04.java
+++ Seminar005/Task04.java
@@ -1,36 +1,36 @@
-package Seminar005;
-
-public class Task04 {
-    /*
-    Взять набор строк, например,
-
-    Мороз и солнце день чудесный
-    Еще ты дремлешь друг прелестный
-    Пора красавица проснись.
-
-    Написать метод, который отсортирует слова по длине с помощью TreeMap.
-    Слова с одинаковой длиной не должны “потеряться”.
-     */
-    public static void main(String[] args) {
-        Map<Integer, List<String>> map = new TreeMap<>(Comparator.reverseOrder());
-        String s = "Мороз и солнце день чудесный\n" +
-                "Еще ты дремлешь друг прелестный\n" +
-                "Пора красавица проснись.";
-        s = s.replace("\n", " ");
-        s = s.replace(".", "");
-        String[] words = s.split(" ");
-        for (String word: words){
-            int len = word.length();
-            if (map.containsKey(len)){
-                List<String> list = map.get(len);
-                list.add(word);
-            } else {
-                List<String> list = new ArrayList<>();
-                list.add(word);
-                map.put(len, list);
-            }
-        }
-
-        System.out.println(map);
-    }
-}
+//package Seminar005;
+//
+//public class Task04 {
+//    /*
+//    Взять набор строк, например,
+//
+//    Мороз и солнце день чудесный
+//    Еще ты дремлешь друг прелестный
+//    Пора красавица проснись.
+//
+//    Написать метод, который отсортирует слова по длине с помощью TreeMap.
+//    Слова с одинаковой длиной не должны “потеряться”.
+//     */
+//    public static void main(String[] args) {
+//        Map<Integer, List<String>> map = new TreeMap<>(Comparator.reverseOrder());
+//        String s = "Мороз и солнце день чудесный\n" +
+//                "Еще ты дремлешь друг прелестный\n" +
+//                "Пора красавица проснись.";
+//        s = s.replace("\n", " ");
+//        s = s.replace(".", "");
+//        String[] words = s.split(" ");
+//        for (String word: words){
+//            int len = word.length();
+//            if (map.containsKey(len)){
+//                List<String> list = map.get(len);
+//                list.add(word);
+//            } else {
+//                List<String> list = new ArrayList<>();
+//                list.add(word);
+//                map.put(len, list);
+//            }
+//        }
+//
+//        System.out.println(map);
+//    }
+//}
